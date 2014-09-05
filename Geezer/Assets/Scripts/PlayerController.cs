5892fb79f3d0e3421c0d182a08b25b232156df2b
Sidharth Juyal <sid@whackylabs.com>
2014-09-05 13:18:54 +0530

    Add PlayerController.cs script

---
+++ Geezer/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+	float movementSpeed = 1.0f;
+	float rotationSpeed = 100.0f;
+	
+	void Update()
+	{
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+		float movement = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+		
+		transform.Translate(0.0f, movement, 0.0f);
+		transform.Rotate(0.0f, 0.0f, -rotation);
+	}
+}
